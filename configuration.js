584159bb234c3ad27bffb31261378853f120ea6a
88x <88x@users.noreply.github.com>
2017-07-27 15:40:47 +0800

    Update configuration.js

--- configuration.js
+++ configuration.js
@@ -11,7 +11,8 @@ angular
   path: '/jsonrpc',
   //port: 6800,
   port: 30011,
-  encrypt: false,
+  //encrypt: false,
+  encrypt: true,
   auth: {                          // either add the token field or the user and pass field, not both.
   // token: '$YOUR_SECRET_TOKEN$'
   token: 'www.qq.com'
